9d33144ea57ab104675d727f849e78027d038cbb
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-01-11 20:28:52 +0200

    Edited classes Page  (added new variable boolean and added 2 constructors of Page class). Initialized array of Pages in the class Story.

--- app/src/main/java/ws/tilda/anastasia/interactivestory/model/Page.java
+++ app/src/main/java/ws/tilda/anastasia/interactivestory/model/Page.java
@@ -5,6 +5,24 @@ public class Page {
     private String mText;
     private Choice mChoice1;
     private Choice mChoice2;
+    private boolean mIsFinal = false;
+
+    public Page(int imageId, String text, Choice choice1, Choice choice2) {
+        mImageId = imageId;
+        mText = text;
+        mChoice1 = choice1;
+        mChoice2 = choice2;
+    }
+
+    public Page(int imageId, String text) {
+        mImageId = imageId;
+        mText = text;
+        mChoice1 = null;
+        mChoice2 = null;
+        mIsFinal = true;
+
+
+    }
 
     public int getImageId() {
         return mImageId;
@@ -37,4 +55,14 @@ public class Page {
     public void setChoice2(Choice choice2) {
         mChoice2 = choice2;
     }
+
+    public boolean isFinal() {
+        return mIsFinal;
+    }
+
+    public void setFinal(boolean aFinal) {
+        mIsFinal = aFinal;
+    }
 }
+
+
