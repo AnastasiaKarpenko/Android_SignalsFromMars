d4ca38f4f2c2d1ba1ed5a16df26545473aa3ba09
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-01-13 12:52:14 +0200

    In StoryActivity class I linked newly created variables with the layout attributes, created method to load new page.

--- app/src/main/java/ws/tilda/anastasia/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/ws/tilda/anastasia/interactivestory/ui/StoryActivity.java
@@ -1,9 +1,14 @@
 package ws.tilda.anastasia.interactivestory.ui;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import ws.tilda.anastasia.interactivestory.R;
 import ws.tilda.anastasia.interactivestory.model.Page;
@@ -11,7 +16,13 @@ import ws.tilda.anastasia.interactivestory.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
     public static final String TAG = StoryActivity.class.getSimpleName();
+
     private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private String mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +30,36 @@ public class StoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        mName = intent.getStringExtra(getString(R.string.key_name));
 
-        if(name == null) {
-            name = "Friend";
+        if(mName == null) {
+            mName = "Friend";
         }
-        Log.d(TAG, name);
+        Log.d(TAG, mName);
+
+        mImageView = (ImageView) findViewById(R.id.storyImageView);
+        mTextView = (TextView) findViewById(R.id.storyTextView);
+        mChoice1 = (Button) findViewById(R.id.choiceButton1);
+        mChoice2 = (Button) findViewById(R.id.choiceButton2);
+
+        loadPage();
     }
 
     public void loadPage() {
         Page page = mStory.getPage(0);
+
+//      Drawable drawable = getResources().getDrawable(page.getImageId()); getDrawable(int) is depreciated
+        Drawable drawable = ContextCompat.getDrawable(this, page.getImageId());
+        mImageView.setImageDrawable(drawable);
+
+        String pageText = page.getText();
+        // This will add the name if placeholder included. Won't add if no placeholder
+        pageText = String.format(pageText, mName);
+        mTextView.setText(pageText);
+
+        mChoice1.setText(page.getChoice1().getText());
+        mChoice2.setText(page.getChoice2().getText());
+
 
     }
 }
